969b5f8f7696349436ac716e140d7ff824a6cbc9
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2024-01-29 16:48:10 -0300

    Update

---
+++ danki_code/exercicios/aula_09_ex/ex009.js
@@ -0,0 +1,7 @@
+console.log('Resultado do exercío ');
+
+const primeiraVariavel = 2148; // A Variável const não consegue/pode ser alterada apos a inicialização.
+let segundaVariavel; // A variável let pode ser alterada após a sua inicialização
+
+console.log(`o valor da primeira variável é ${valor}`);
+
